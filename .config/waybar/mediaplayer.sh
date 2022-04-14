2f2ec564afb5fa9ff741ad599a08fab2e4200dc5
ichbinram <ramprasadraghunath@gmail.com>
2022-04-14 06:07:22 +0200

    changed spotify module to use zsh instead of bash

--- .config/waybar/mediaplayer.sh
+++ .config/waybar/mediaplayer.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/zsh
 player_status=$(playerctl status 2> /dev/null)
 if [ "$player_status" = "Playing" ]; then
-    echo "$(playerctl metadata artist) - $(playerctl metadata title)"
-elif [ "$player_status" = "Paused" ]; then
     echo " $(playerctl metadata artist) - $(playerctl metadata title)"
+elif [ "$player_status" = "Paused" ]; then
+    echo " $(playerctl metadata artist) - $(playerctl metadata title)"
 fi
 
